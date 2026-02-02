c76bd11c9be4b75e277e1958ad1d10aaf9e5de4c
pranavsirsufale <pranavdada334@gmail.com>
2026-02-02 22:19:41 +0530

    change database configuration in settings.py for mysql database

--- csitAttendance/csitAttendance/settings.py
+++ csitAttendance/csitAttendance/settings.py
@@ -42,12 +42,21 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'csitAttendance.wsgi.application'
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "csit",
+        "USER": "root",
+        "PASSWORD": "pass@123",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
